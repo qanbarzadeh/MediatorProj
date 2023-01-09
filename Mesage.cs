34ffd4d35df6cbecff9e7784c1b1b92f3cbb2442
qanbarzadeh <ghxalireza@gmail.com>
2023-01-10 00:03:09 +0800

    Create Test class and handler methods

--- Mesage.cs
+++ Mesage.cs
@@ -3,15 +3,15 @@
     public class Message
     {
         public IColleague Sender { get; private set; }
-        public string MessageContent { get; private set; }
+        public string messageContent { get; private set; }
 
         public Message(string messageContent, IColleague sender)
         {
             Sender = sender;
-            MessageContent = messageContent;
+            this.messageContent = messageContent;
 
             Sender = sender ?? throw new ArgumentNullException(nameof(sender));
-            MessageContent = messageContent ?? throw new ArgumentNullException(nameof(messageContent));
+            this.messageContent = messageContent ?? throw new ArgumentNullException(nameof(messageContent));
         }
 
         
